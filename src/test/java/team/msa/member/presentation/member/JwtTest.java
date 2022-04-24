9aef885e441a2783070bae7bf28fb71b2b73db04
MILL_WHITE <hayanyoo.dev@gmail.com>
2022-04-24 21:04:36 +0900

    feat jwt 생성 메소드 테스트

---
+++ src/test/java/team/msa/member/presentation/member/JwtTest.java
@@ -0,0 +1,36 @@
+package team.msa.member.presentation.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import team.msa.member.domain.model.member.Member;
+import team.msa.member.domain.model.member.MemberType;
+import team.msa.member.infrastructure.jwt.JwtProvider;
+
+@SpringBootTest
+public class JwtTest {
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Value("${jwt.refreshExpires}")
+    private String refreshExpires;
+
+    @Test
+    void generateToken() {
+        Member member = Member.builder()
+                .memberId(1)
+                .memberName("memberName")
+                .memberPassword("memberPassword")
+                .memberType(MemberType.ADMIN)
+                .build();
+        long accessExpires = Long.parseLong(refreshExpires);
+
+        String token = jwtProvider.createJwtToken(member, accessExpires);
+        System.out.println(token);
+
+    }
+
+
+}
