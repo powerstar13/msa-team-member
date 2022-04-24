87831f08a76bff9cd6037d2bec512e672856b185
huisoo <huiwater@gmail.com>
2022-04-24 16:54:00 +0900

    중복 멤버 체크 로직 추가

--- src/main/java/team/msa/member/application/member/MemberApplicationServiceImpl.java
+++ src/main/java/team/msa/member/application/member/MemberApplicationServiceImpl.java
@@ -1,11 +1,31 @@
 package team.msa.member.application.member;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
+import reactor.core.publisher.Mono;
 import team.msa.member.application.response.MemberBlahBlahResponse;
+import team.msa.member.domain.model.member.Member;
+import team.msa.member.domain.model.member.MemberFactory;
+import team.msa.member.domain.model.member.MemberRepository;
 
 @Service
+@RequiredArgsConstructor
 public class MemberApplicationServiceImpl implements MemberApplicationService {
+
+    private final MemberRepository memberRepository;
+    private final MemberFactory memberFactory;
+
+    private void validateDuplicateMember(Member member) {
+        //Exception
+        Mono<Boolean> duplicatYn = memberRepository.existsById(member.getMemberId());
+        duplicatYn.flatMap(isExist -> {
+            if(isExist){
+                Mono.error(new IllegalStateException("duplicate member!!"));
+            }
+            return Mono.just(isExist);
+        });
+    }
 
     @Override
     public MemberBlahBlahResponse teacherRegistration(ServerRequest request) {
