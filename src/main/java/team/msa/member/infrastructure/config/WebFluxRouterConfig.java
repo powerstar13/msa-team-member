b2011dba456fb89e70c207732994f22d4d6472c8
홍준성 <powerstar13@hanmail.net>
2022-04-25 21:37:11 +0900

    Thread 분리 위해 boundedElastic 스케쥴러 처리 및 Handler 병합

--- src/main/java/team/msa/member/infrastructure/config/WebFluxRouterConfig.java
+++ src/main/java/team/msa/member/infrastructure/config/WebFluxRouterConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import team.msa.member.presentation.member.AdminHandler;
 import team.msa.member.presentation.member.MemberHandler;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
@@ -17,24 +16,6 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 @EnableWebFlux // WebFlux 설정 활성화
 public class WebFluxRouterConfig implements WebFluxConfigurer {
 
-    /**
-     * 사이트 운영자 전용 Router
-     * @param adminHandler : 관리자
-     * @return RouterFunction<ServerResponse> : 빌드된 Router
-     */
-    @Bean
-    public RouterFunction<ServerResponse> adminRouterBuilder(AdminHandler adminHandler) {
-
-        return RouterFunctions.route()
-            .path("/admin", adminBuilder ->
-                adminBuilder.nest(accept(MediaType.APPLICATION_JSON), builder ->
-                    builder
-                        .POST("/teacherRegistration", adminHandler::teacherRegistration) // 강사 등록 (관리자만)
-                )
-            )
-            .build();
-    }
-
     @Bean
     public RouterFunction<ServerResponse> memberRouterBuilder(MemberHandler memberHandler) {
 
@@ -42,6 +23,7 @@ public class WebFluxRouterConfig implements WebFluxConfigurer {
             .path("/member", memberBuilder ->
                 memberBuilder.nest(accept(MediaType.APPLICATION_JSON), builder ->
                     builder
+                        .POST("/admin/teacherRegistration", memberHandler::teacherRegistration) // 강사 등록 (관리자만)
                         .POST("/studentRegistration", memberHandler::studentRegistration) // 학생 회원 가입
                         .POST("/login", memberHandler::login) // 로그인
                 )
--- src/main/java/team/msa/member/presentation/member/MemberHandler.java
+++ src/main/java/team/msa/member/presentation/member/MemberHandler.java
@@ -24,6 +24,22 @@ public class MemberHandler {
     private final MemberApplicationService memberApplicationService;
 
     /**
+     * 강사 등록
+     * --> '사이트 운영자'는 강의 컨테츠를 업로드할 '강사' 회원을 생성할 수 있다.
+     * @param request : 등록할 강사 정보
+     * @return Mono<ServerResponse> : 등록된 강사 정보
+     */
+    public Mono<ServerResponse> teacherRegistration(ServerRequest request) {
+
+        Mono<MemberRegistrationResponse> response = memberApplicationService.memberRegistration(request, MemberType.TEACHER)
+            .subscribeOn(Schedulers.boundedElastic());
+
+        return ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(response, MemberRegistrationResponse.class);
+    }
+
+    /**
      * 학생 회원 가입
      * --> 강의를 수강하고자 하는 사람은 '학생'으로 회원 가입이 가능하다.
      * @param request : 가입할 학생 정보
@@ -31,7 +47,8 @@ public class MemberHandler {
      */
     public Mono<ServerResponse> studentRegistration(ServerRequest request) {
 
-        Mono<MemberRegistrationResponse> response = memberApplicationService.memberRegistration(request, MemberType.STUDENT);
+        Mono<MemberRegistrationResponse> response = memberApplicationService.memberRegistration(request, MemberType.STUDENT)
+            .subscribeOn(Schedulers.boundedElastic());
 
         return ok()
             .contentType(MediaType.APPLICATION_JSON)
