a07acbf8f98bd4114f0a9dc4e69a923d5c41e517
홍준성 <powerstar13@hanmail.net>
2022-04-26 01:59:46 +0900

    Eureka Client 등록

--- src/main/java/team/msa/member/MsaTeamMemberApplication.java
+++ src/main/java/team/msa/member/MsaTeamMemberApplication.java
@@ -2,9 +2,10 @@ package team.msa.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-//@EnableEurekaClient // Eureka Client 역할을 수행한다. YAML에 명시한 유레카 서버에 해당 애플리케이션을 등록한다.
+@EnableDiscoveryClient // Eureka Client 역할을 수행한다. YAML에 명시한 유레카 서버에 해당 애플리케이션을 등록한다.
 public class MsaTeamMemberApplication {
 
 	public static void main(String[] args) {
